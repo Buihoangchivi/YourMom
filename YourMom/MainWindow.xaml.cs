2a8148724b476f2c0be7f66b502ec762baed5bbc
Vĩ Bùi <buivankhang9@gmail.com>
2021-06-04 10:47:36 +0700

    Tao layout co ban cho thanh menu

--- YourMom/MainWindow.xaml.cs
+++ YourMom/MainWindow.xaml.cs
@@ -24,5 +24,115 @@ namespace YourMom
 		{
 			InitializeComponent();
 		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		//---------------------------------------- Các hàm xử lý sự kiện --------------------------------------------//
+
+
+
+		//Cài đặt để có thể di chuyển cửa sổ khi nhấn giữ chuột và kéo Title Bar
+		private void DockPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			var move = sender as System.Windows.Controls.DockPanel;
+			var win = Window.GetWindow(move);
+			win.DragMove();
+		}
+
+
+		//---------------------------------------- Xử lý cửa sổ --------------------------------------------//
+
+		//Cài đặt nút đóng cửa sổ
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			/*SaveListFood();
+			SaveListDish();
+			var config = ConfigurationManager.OpenExeConfiguration(
+				ConfigurationUserLevel.None);
+			config.AppSettings.Settings["ColorScheme"].Value = ColorScheme;
+			config.Save(ConfigurationSaveMode.Minimal);*/
+			Application.Current.Shutdown();
+
+		}
+		//Cài đặt nút phóng to/ thu nhỏ cửa sổ
+		private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+		{
+			AdjustWindowSize();
+		}
+
+		//Cài đặt nút ẩn cửa sổ
+		private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
+
+		//Thay đổi kích thước cửa sổ
+		//Nếu đang ở trạng thái phóng to thì thu nhỏ và ngược lại
+		private void AdjustWindowSize()
+		{
+			var imgName = "";
+
+			if (WindowState == WindowState.Maximized)
+			{
+				WindowState = WindowState.Normal;
+				imgName = "Images/maximize.png";
+			}
+			else
+			{
+				WindowState = WindowState.Maximized;
+				imgName = "Images/restoreDown.png";
+			}
+
+			//Lấy nguồn ảnh
+			var img = new BitmapImage(new Uri(
+						imgName,
+						UriKind.Relative)
+				);
+
+			//Thiết lập ảnh chất lượng cao
+			RenderOptions.SetBitmapScalingMode(img, BitmapScalingMode.HighQuality);
+
+			//Thay đổi icon
+			(MaxButton.Content as Image).Source = img;
+		}
+
+
+		private void BackButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void MenuButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void TransactionsButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ReportButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void BudgetButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void DebtsButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void SettingButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
 	}
 }
