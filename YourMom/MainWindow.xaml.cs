f5ed369cf65a9a51515f7cadb0a0a80407982da3
Vĩ Bùi <buivankhang9@gmail.com>
2021-06-06 11:29:29 +0700

    Tao 2 bieu do hinh quat the hien bao cao ve thu chi.

--- YourMom/MainWindow.xaml.cs
+++ YourMom/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,14 @@ namespace YourMom
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		
+		int[] arr = { 2, 5, 6, 7, 9, 10 };
+
 		public MainWindow()
 		{
+
 			InitializeComponent();
+			
 		}
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -157,6 +164,42 @@ namespace YourMom
 
 				GeneralReportGrid.Width = 800;
 
+			}
+
+		}
+
+		//Biểu đồ hình quạt về thu nhập
+		private void IncomeReportChart_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+
+			IncomeReportChart.Series = new SeriesCollection();
+			((DefaultTooltip)IncomeReportChart.DataTooltip).SelectionMode = TooltipSelectionMode.OnlySender;
+			foreach (var member in arr)
+			{
+				IncomeReportChart.Series.Add(
+						new PieSeries()
+						{
+							Values = new ChartValues<decimal> { member }
+						}
+					); ;
+			}
+
+		}
+
+		//Biểu đồ hình quạt về chi tiêu
+		private void ExpenseReportChart_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+
+			ExpenseReportChart.Series = new SeriesCollection();
+			((DefaultTooltip)ExpenseReportChart.DataTooltip).SelectionMode = TooltipSelectionMode.OnlySender;
+			foreach (var member in arr)
+			{
+				ExpenseReportChart.Series.Add(
+						new PieSeries()
+						{
+							Values = new ChartValues<decimal> { member }
+						}
+					); ;
 			}
 
 		}
