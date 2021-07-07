335fd760647429fc7c8d7b82e117c1a1fb5c4e0b
Vĩ Bùi <buivankhang9@gmail.com>
2021-07-07 12:32:43 +0700

    Cài đặt xử lý và thay đổi UI của 3 nút chuyển thời gian xem danh sách giao dịch.

--- YourMom/MainWindow.xaml.cs
+++ YourMom/MainWindow.xaml.cs
@@ -35,6 +35,7 @@ namespace YourMom
 
         private List<Transaction> transactionList;
         private Dictionary<string, Category> categoryList = new Dictionary<string, Category>();
+        private DateTime startingDate, endDate;
 
         List<DetailCategory> incomeList = new List<DetailCategory>();
         List<DetailCategory> expenseList = new List<DetailCategory>();
@@ -395,8 +396,17 @@ namespace YourMom
             AddDataIntoList(debtPairs, debtList);
             AddDataIntoList(loanPairs, loanList);
 
-            DateTime startingDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
-            DateTime endDate = startingDate.AddMonths(1).AddDays(-1);
+            startingDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            endDate = startingDate.AddMonths(1).AddDays(-1);
+            AddDataIntoTransactionScreen();
+
+        }
+
+        private void AddDataIntoTransactionScreen()
+        {
+
+            categoryCollection = new ObservableCollection<CategoryList>();
+            transactionCollection = new ObservableCollection<TransactionList>();
 
             //Đọc dữ liệu tất cả các giao dịch vào danh sách giao dịch ở dạng nhóm
             InitDataIntoObservableCollection(categoryCollection, startingDate, endDate);
@@ -420,7 +430,8 @@ namespace YourMom
             {
 
                 //Kiểm tra xem thời gian của giao dịch có nằm trong khoảng thời thời đầu vào hay không
-                if (startingDate <= transaction.Date && transaction.Date <= endDate)
+                if (startingDate <= transaction.Date &&
+                    (transaction.Date <= endDate || endDate == DateTime.MaxValue))
                 {
 
                     //ID của loại giao dịch
@@ -511,6 +522,13 @@ namespace YourMom
                 return;
 
             }
+            else //Nếu có giao dịch thì hiển thị khung danh sách giao dịch
+            {
+
+                TransactionScrollViewer.Visibility = Visibility.Visible;
+                NoTransactionBorder.Visibility = Visibility.Collapsed;
+
+            }
 
             //Định dạng lại số tiền ở dạng chuỗi và truyền vào màn hình
             Modal.MoneyConverter moneyConverter = new Modal.MoneyConverter();
@@ -541,7 +559,8 @@ namespace YourMom
             {
 
                 //Kiểm tra xem thời gian của giao dịch có nằm trong khoảng thời thời đầu vào hay không
-                if (startingDate <= transaction.Date && transaction.Date <= endDate)
+                if (startingDate <= transaction.Date &&
+                    (transaction.Date <= endDate || endDate == DateTime.MaxValue))
                 {
 
                     //ID của loại giao dịch
@@ -635,6 +654,13 @@ namespace YourMom
                 TransactionScrollViewer.Visibility = Visibility.Collapsed;
                 NoTransactionBorder.Visibility = Visibility.Visible;
                 return;
+
+            }
+            else //Nếu có giao dịch thì hiển thị khung danh sách giao dịch
+            {
+
+                TransactionScrollViewer.Visibility = Visibility.Visible;
+                NoTransactionBorder.Visibility = Visibility.Collapsed;
 
             }
 
@@ -1039,309 +1065,10 @@ namespace YourMom
 
             }
 
-
-
-            /*List<Category> categoryList = new List<Category>
-			{
-				new Category
-				{
-					ID = "0",
-					Name = "Eating",
-					ImagePath = "Images\\Eating.jpg"
-				},
-				new Category
-				{
-					ID = "1",
-					Name = "Shopping",
-					ImagePath = "Images\\Shopping.jpg"
-				},
-				new Category
-				{
-					ID = "2",
-					Name = "Bills",
-					ImagePath = "Images\\Bills.jpg"
-				},
-				new Category
-				{
-					ID = "3",
-					Name = "Entertainment",
-					ImagePath = "Images\\Entertainment.jpg"
-				},
-			};*/
-
-            /*List<DetailCategory> detailCategoryList = new List<DetailCategory>
-			{
-				new DetailCategory
-				{
-					ID = "0",
-					Name = "Eating",
-					ImagePath = "Images\\Eating.jpg",
-					Amount = 1500000
-				},
-				new DetailCategory
-				{
-					ID = "1",
-					Name = "Shopping",
-					ImagePath = "Images\\Shopping.jpg",
-					Amount = 300000
-				},
-				new DetailCategory
-				{
-					ID = "2",
-					Name = "Bills",
-					ImagePath = "Images\\Bills.jpg",
-					Amount = 760000
-				},
-				new DetailCategory
-				{
-					ID = "3",
-					Name = "Entertainment",
-					ImagePath = "Images\\Entertainment.jpg",
-					Amount = 100000
-				}
-			};*/
-
-            /*List<Budget> budgetList = new List<Budget>
-			{
-				new Budget
-				{
-					ID = "0",
-					Name = "Eating",
-					ImagePath = "Images\\Eating.jpg",
-					StartingDate = "6/7/2021 11:14:17 AM",
-					EndDate = "7/20/2021 12:17:00 AM",
-					MoneyFund = 2000000,
-					Note = "Tiền ăn uống cho nửa tháng"
-				},
-				new Budget
-				{
-					ID = "1",
-					Name = "Shopping",
-					ImagePath = "Images\\Shopping.jpg",
-					StartingDate = "5/7/2021 4:00:00 AM",
-					EndDate = "6/7/2021 12:00:00 AM",
-					MoneyFund = 2000000,
-					Note = "Tiền mua sắm cho một tháng"
-				},
-				new Budget
-				{
-					ID = "2",
-					Name = "Bills",
-					ImagePath = "Images\\Bills.jpg",
-					StartingDate = "3/10/2021 10:14:17 AM",
-					EndDate = "3/12/2021 10:17:00 AM",
-					MoneyFund = 2000000,
-					Note = "Tiền hóa đơn cho 2 ngày"
-				},
-				new Budget
-				{
-					ID = "3",
-					Name = "Entertainment",
-					ImagePath = "Images\\Entertainment.jpg",
-					StartingDate = "2/22/2020 16:00:00 AM",
-					EndDate = "2/22/2021 16:00:00 AM",
-					MoneyFund = 2000000,
-					Note = "Tiền giải trí cho một năm"
-				}
-			};*/
-
-            /*List<DebitBook> debitBookList = new List<DebitBook>
-			{
-				new DebitBook
-				{
-					ID = "14",
-					Name = "Lending",
-					ImagePath = "Images\\Lending.jpg",
-					StartingDate = "6/7/2021 11:14:17 AM",
-					EndDate = "7/20/2021 12:17:00 AM",
-					Amount = 500000,
-					Note = "Tiền mượn đi ăn sáng",
-					Stakeholder = "Phạm Tấn" 
-				},
-				new DebitBook
-				{
-					ID = "15",
-					Name = "Paying",
-					ImagePath = "Images\\Paying.jpg",
-					StartingDate = "5/7/2021 4:00:00 AM",
-					EndDate = "6/7/2021 12:00:00 AM",
-					Amount = 250000,
-					Note = "Trả tiền mượn đi ăn sáng",
-					Stakeholder = "Phạm Tấn"
-				},
-				new DebitBook
-				{
-					ID = "16",
-					Name = "Borrowing",
-					ImagePath = "Images\\Borrowing.jpg",
-					StartingDate = "3/10/2021 10:14:17 AM",
-					EndDate = "3/12/2021 10:17:00 AM",
-					Amount = 60000,
-					Note = "Cho mượn tiền đi đổ xăng",
-					Stakeholder = "Bùi Văn Vĩ"
-				},
-				new DebitBook
-				{
-					ID = "17",
-					Name = "DebtCollection",
-					ImagePath = "Images\\DebtCollection.jpg",
-					StartingDate = "2/22/2020 16:00:00 AM",
-					Amount = 24000,
-					Note = "Thu nợ tiền đi đổ xăng",
-					Stakeholder = "Bùi Văn Vĩ"
-				}
-			};*/
-
-            /*List<Transaction> transactionList = new List<Transaction>
-			{
-				new Transaction
-				{
-					ID = "abc111",
-					Date = "6/7/2021 11:14:17 AM",
-					TransactionType = "0",
-					Amount = 50000,
-					Note = "Tiền đi ăn sáng ở quán bún đậu mắm tôm",
-					Stakeholder = "Phạm Tấn"
-				},
-				new Transaction
-				{
-					ID = "hyz423",
-					Date = "3/10/2021 10:14:17 AM",
-					TransactionType = "1",
-					Amount = 230000,
-					Note = "Tiền đi mua sắm ở siêu thị",
-					Stakeholder = "Bùi Văn Vĩ"
-				},
-				new Transaction
-				{
-					ID = "xyz473",
-					Date = "2/22/2020 16:00:00 AM",
-					TransactionType = "2",
-					Amount = 100000,
-					Note = "Tiền điện tháng 7 ở phòng KTX",
-					Stakeholder = "Nguyễn Huy Hải"
-				},
-				new Transaction
-				{
-					ID = "uhd723",
-					Date = "3/12/2021 10:17:00 AM",
-					TransactionType = "3",
-					Amount = 96000,
-					Note = "Tiền đi uống cà phê ở quán Highland",
-					Stakeholder = "Nguyễn Công Phượng"
-				}
-			};*/
-
-            /*List<Report> reports = new List<Report>
-			{
-				new Report
-				{
-					ID = "a28hdw23jou4",
-					StartingDate = "6/7/2021 11:14:17 AM",
-					EndDate = "7/20/2021 12:17:00 AM",
-					Income = new List<DetailCategory>
-					{
-						new DetailCategory
-						{
-							ID = "25",
-							Name = "Bonus",
-							ImagePath = "Images\\Bonus.jpg",
-							Amount = 1500000
-						},
-						new DetailCategory
-						{
-							ID = "26",
-							Name = "Interest",
-							ImagePath = "Images\\Interest.jpg",
-							Amount = 300000
-						},
-						new DetailCategory
-						{
-							ID = "27",
-							Name = "Salary",
-							ImagePath = "Images\\Salary.jpg",
-							Amount = 760000
-						},
-						new DetailCategory
-						{
-							ID = "28",
-							Name = "Awarded",
-							ImagePath = "Images\\Awarded.jpg",
-							Amount = 100000
-						}
-					},
-					Expense = new List<DetailCategory>
-					{
-						new DetailCategory
-						{
-							ID = "0",
-							Name = "Eating",
-							ImagePath = "Images\\Eating.jpg",
-							Amount = 1500000
-						},
-						new DetailCategory
-						{
-							ID = "1",
-							Name = "Shopping",
-							ImagePath = "Images\\Shopping.jpg",
-							Amount = 300000
-						},
-						new DetailCategory
-						{
-							ID = "2",
-							Name = "Bills",
-							ImagePath = "Images\\Bills.jpg",
-							Amount = 760000
-						},
-						new DetailCategory
-						{
-							ID = "3",
-							Name = "Entertainment",
-							ImagePath = "Images\\Entertainment.jpg",
-							Amount = 100000
-						}
-					},
-					Debt = new List<DetailCategory>
-					{
-						new DetailCategory
-						{
-							ID = "15",
-							Name = "Paying",
-							ImagePath = "Images\\Paying.jpg",
-							Amount = 300000
-						},
-						new DetailCategory
-						{
-							ID = "16",
-							Name = "Borrowing",
-							ImagePath = "Images\\Borrowing.jpg",
-							Amount = 760000
-						}
-					},
-					Loan = new List<DetailCategory>
-					{
-						new DetailCategory
-						{
-							ID = "14",
-							Name = "Lending",
-							ImagePath = "Images\\Lending.jpg",
-							Amount = 1500000
-						},
-						new DetailCategory
-						{
-							ID = "17",
-							Name = "DebtCollection",
-							ImagePath = "Images\\DebtCollection.jpg",
-							Amount = 100000
-						}
-					}
-				}
-			};
-
-			XmlSerializer xs = new XmlSerializer(typeof(List<Report>));
-			TextWriter writer = new StreamWriter(@"Data\Report.xml");
-			xs.Serialize(writer, reports);
-			writer.Close();*/
+            //XmlSerializer xs = new XmlSerializer(typeof(List<Report>));
+            //TextWriter writer = new StreamWriter(@"Data\Report.xml");
+            //xs.Serialize(writer, reports);
+            //writer.Close();
 
         }
 
@@ -1844,25 +1571,50 @@ namespace YourMom
         private void PreviousButton_Click(object sender, RoutedEventArgs e)
         {
 
-            //Chuyển nút trước đó sang trạng thái được chọn
-            ChangeButtonStatus(PreviousDash, PreviousTextBlock, true);
-            //Chuyển nút hiện tại sang trạng thái không được chọn
-            ChangeButtonStatus(CurrentDash, CurrentTextBlock, false);
-            //Chuyển nút tiếp theo sang trạng thái không được chọn
-            ChangeButtonStatus(NextDash, NextTextBlock, false);
+            //Trường hợp đang ở trạng thái tháng tương lai
+            if (CurrentDash.Background != Brushes.Green)
+            {
+
+                //Chuyển nút hiện tại sang trạng thái được chọn
+                ChangeButtonStatus(CurrentDash, CurrentTextBlock, true);
+                //Chuyển nút tiếp theo sang trạng thái không được chọn
+                ChangeButtonStatus(NextDash, NextTextBlock, false);
+
+                //Giảm khoảng thời gian bắt đầu xuống tháng hiện tại
+                startingDate = startingDate.AddMonths(-1);
+
+            }
+
+            //Giảm khoảng thời gian bắt đầu xuống tháng trước
+            startingDate = startingDate.AddMonths(-1);
+            endDate = startingDate.AddMonths(1).AddDays(-1);
+
+            //Thay đổi tên các nút hiển thị khoảng thời gian
+            ChangeTitleOfDateButton();
 
         }
 
-        // Nút tháng hiện tại, có thể dùng để chuyển sang tháng tiếp theo với tháng trước 
+        // Nút chuyển sang tháng hiện tại trong giao diện giao dịch
         private void CurrentButton_Click(object sender, RoutedEventArgs e)
         {
 
-            //Chuyển nút trước đó sang trạng thái không được chọn
-            ChangeButtonStatus(PreviousDash, PreviousTextBlock, false);
-            //Chuyển nút hiện tại sang trạng thái được chọn
-            ChangeButtonStatus(CurrentDash, CurrentTextBlock, true);
-            //Chuyển nút tiếp theo sang trạng thái không được chọn
-            ChangeButtonStatus(NextDash, NextTextBlock, false);
+            //Trường hợp đang ở trạng thái tháng tương lai
+            if (CurrentDash.Background != Brushes.Green)
+            {
+
+                //Chuyển nút hiện tại sang trạng thái được chọn
+                ChangeButtonStatus(CurrentDash, CurrentTextBlock, true);
+                //Chuyển nút tiếp theo sang trạng thái không được chọn
+                ChangeButtonStatus(NextDash, NextTextBlock, false);
+
+                //Khoảng thời gian là tháng hiện tại
+                startingDate = startingDate.AddMonths(-1);
+                endDate = startingDate.AddMonths(1).AddDays(-1);
+
+                //Thay đổi tên các nút hiển thị khoảng thời gian
+                ChangeTitleOfDateButton();
+
+            }
 
         }
 
@@ -1870,12 +1622,94 @@ namespace YourMom
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
 
-            //Chuyển nút trước đó sang trạng thái không được chọn
-            ChangeButtonStatus(PreviousDash, PreviousTextBlock, false);
-            //Chuyển nút hiện tại sang trạng thái không được chọn
-            ChangeButtonStatus(CurrentDash, CurrentTextBlock, false);
-            //Chuyển nút tiếp theo sang trạng thái được chọn
-            ChangeButtonStatus(NextDash, NextTextBlock, true);
+            //Trường hợp không ở trạng thái tháng tương lai
+            if (NextDash.Background != Brushes.Green)
+            {
+
+                //Tăng thời gian bắt đầu lên 1 tháng
+                startingDate = startingDate.AddMonths(1);
+                endDate = startingDate.AddMonths(1).AddDays(-1);
+
+                //Thay đổi tên các nút hiển thị khoảng thời gian
+                ChangeTitleOfDateButton();
+
+            }
+
+        }
+
+        //Thay đổi tiêu đề của 3 nút trạng thái thời gian
+        private void ChangeTitleOfDateButton()
+        {
+
+            //Thời gian bắt đầu của tháng hiện tại theo thời gian thực
+            var startingOfThisMonth = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+
+            //Thời gian bắt đầu của tháng trước
+            var lastStartingDate = startingDate.AddMonths(-1);
+            //Thời gian kết thúc của tháng trước
+            var lastEndDate = lastStartingDate.AddMonths(1).AddDays(-1);
+            //Thời gian bắt đầu của tháng tiếp theo
+            var nextStartingDate = startingDate.AddMonths(1);
+            //Thời gian kết thúc của tháng tiếp theo
+            var nextEndDate = nextStartingDate.AddMonths(1).AddDays(-1);
+
+            //Hiển thị tên 3 nút
+            PreviousTextBlock.Text =
+                $"{lastStartingDate.Day}/{lastStartingDate.Month}/{lastStartingDate.Year}" +
+                $" - {lastEndDate.Day}/{lastEndDate.Month}/{lastEndDate.Year}";
+
+            CurrentTextBlock.Text =
+                $"{startingDate.Day}/{startingDate.Month}/{startingDate.Year}" +
+                $" - {endDate.Day}/{endDate.Month}/{endDate.Year}";
+
+            NextTextBlock.Text =
+                $"{nextStartingDate.Day}/{nextStartingDate.Month}/{nextStartingDate.Year}" +
+                $" - {nextEndDate.Day}/{nextEndDate.Month}/{nextEndDate.Year}";
+
+            //Kiểm tra có phải là 2 tháng trước hay không
+            if (startingDate == startingOfThisMonth.AddMonths(-2))
+            {
+
+                //Tháng tiếp theo chính là tháng trước
+                NextTextBlock.Text = "LAST MONTH";
+
+            }
+            //Kiểm tra có phải là tháng trước hay không
+            else if (startingDate == startingOfThisMonth.AddMonths(-1))
+            {
+
+                //Tháng hiện tại chính là tháng trước
+                CurrentTextBlock.Text = "LAST MONTH";
+                //Tháng tiếp theo chính là tháng này
+                NextTextBlock.Text = "THIS MONTH";
+
+            }
+            //Kiểm tra có phải là tháng hiện tại trở đi hay không
+            else if (startingDate >= startingOfThisMonth)
+            {
+
+                PreviousTextBlock.Text = "LAST MONTH";
+                CurrentTextBlock.Text = "THIS MONTH";
+                NextTextBlock.Text = "FUTURE";
+
+                //Đang ở trạng thái tháng tương lai
+                if (startingDate > startingOfThisMonth)
+                {
+
+                    //Thời gian kết thúc là vô cùng
+                    endDate = DateTime.MaxValue;
+
+                    //Chuyển nút hiện tại sang trạng thái không được chọn
+                    ChangeButtonStatus(CurrentDash, CurrentTextBlock, false);
+                    //Chuyển nút tiếp theo sang trạng thái được chọn
+                    ChangeButtonStatus(NextDash, NextTextBlock, true);
+
+                }
+
+            }
+
+            //Đưa dữ liệu vào màn hình danh sách giao dịch
+            AddDataIntoTransactionScreen();
 
         }
 
